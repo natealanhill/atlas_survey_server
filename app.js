f88cdfede5817b8a5e380003c6ddaf319f147d44
Ryan <ryaneckere1@gmail.com>
2021-05-26 18:04:19 -0400

    results model added

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ let express = require('express');
 let app = express();
 const sequelize = require('./db');
 
-let user = require('./controllers/usercontroller'); 
-let survey = require('./controllers/logcontroller'); 
-let info = require('./controllers/userinfocontroller');
+let admin = require('./controllers/admincontroller'); 
+let survey = require('./controllers/surveycontroller'); 
+let results = require('./controllers/resultscontroller');
 
 sequelize.sync();
-// sequelize.sync({force: true})
+//sequelize.sync({force: true})
 app.use(require('./middleware/headers'));
 
 app.use(express.json());
 
-app.use('/user', user);
+app.use('/admin', admin);
 
 app.use('/survey', survey);
 
-app.use('/info', info);
+app.use('/results', results);
 
 app.listen(3030, function() {
     console.log("App is listening on port 3030");
